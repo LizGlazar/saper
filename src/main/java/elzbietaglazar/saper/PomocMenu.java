3eb32a80747de76eb4356914fca4aadb8a88fd0c
Elzbieta <elzbieta.glazar@gmail.com>
2019-10-19 22:45:19 +0200

    Okienko informacyjne do elementu menu "O programie"

--- src/main/java/elzbietaglazar/saper/PomocMenu.java
+++ src/main/java/elzbietaglazar/saper/PomocMenu.java
@@ -1,6 +1,8 @@
 package elzbietaglazar.saper;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PomocMenu extends JMenu
 {
@@ -9,5 +11,13 @@ public class PomocMenu extends JMenu
         super("Pomoc");
         JMenuItem oProgramie = new JMenuItem("O programie");
         add(oProgramie);
+        oProgramie.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ae)
+            {
+                JOptionPane.showMessageDialog(PomocMenu.this, "Elżbieta Glazar \u2014 Saper",
+                        "Programowanie obiektowe", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
     }
 }
